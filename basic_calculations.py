31e3ae31c89322c361443c020d5cca8880915afc
Rachel Xie <rxie2002@gmail.com>
2021-08-06 12:10:53 -0400

    Added ability to check if mass slips down ramp

--- basic_calculations.py
+++ basic_calculations.py
@@ -1,14 +1,23 @@
 """Some simple calculations needed for the friction simulator.
 
-Sources for the value of mu were https://www.engineersedge.com/coeffients_of_friction.htm
-and Nelson Physics 11 textbook. Where ranges were given, the lowest value was chosen. For
-the value of aluminum on ice, the recommended value from The Friction of Saline Ice on
-Aluminum by Christopher Wallen-Russell and Ben Lishman.
+Sources for the value of mu in coefficients.csv and MATERIALS
+were https://www.engineersedge.com/coeffients_of_friction.htm
+and Nelson Physics 11 textbook. Where ranges were given,
+the lowest value was chosen. For the value of aluminum on ice,
+the recommended value from The Friction of Saline Ice on
+Aluminum by Christopher Wallen-Russell and Ben Lishman was used.
 """
 import math
+from typing import List
 
 # CONSTANTS
 GRAVITY = 9.8  # m/s^2
+MATERIALS = {
+    'aluminum': {'steel': 0.61, 'aluminum': 1.1, 'wood': 0.2, 'ice': 0.1},
+    'steel': {'steel': 0.78, 'aluminum': 0.61, 'wood': 0.2, 'ice': 0.1},
+    'wood': {'steel': 0.2, 'aluminum': 0.2, 'wood': 0.25, 'ice': 0.05},
+    'ice': {'steel': 0.1, 'aluminum': 0.1, 'wood': 0.05, 'ice': 0.1}
+}
 
 
 def get_ramp_height(ramp_angle: float, screen_width: int) -> float:
@@ -16,7 +25,8 @@ def get_ramp_height(ramp_angle: float, screen_width: int) -> float:
     on which it will be displayed.
 
     Preconditions:
-        - 0 <= ramp_angle <= 90
+        - 0 <= ramp_angle < 90
+        - 0 < screen_width
     """
     rad_angle = math.radians(ramp_angle)
 
@@ -28,7 +38,8 @@ def get_ramp_length(ramp_angle: float, screen_width: int) -> float:
     given ramp_angle and screen_width.
 
     Preconditions:
-        - 0 <= ramp_angle <= 90
+        - 0 <= ramp_angle < 90
+        - 0 < screen_width
     """
     base = screen_width / 2
     height = get_ramp_height(ramp_angle, screen_width)
@@ -36,43 +47,74 @@ def get_ramp_length(ramp_angle: float, screen_width: int) -> float:
     return math.sqrt(base ** 2 + height ** 2)
 
 
-def get_weight(m: float, g: float = GRAVITY) -> float:
-    """Return the weight of the object with the given mass m, as well as its components."""
-    return m * g
+def calculate_if_slips(mass: float, materials: List[str], ramp_angle: float,
+                       g: float = GRAVITY) -> bool:
+    """Return whether an object of the given mass will slip under the given conditions.
+
+    Preconditions:
+        - 0 <= ramp_angle < 90
+        - 0 < mass
+        - len(materials) == 2
+        - materials[0] in {'ice', 'steel', 'wood', 'aluminum'} and
+            materials[1] in {'ice', 'steel', 'wood', 'aluminum'}
+    """
+    rad_angle = math.radians(ramp_angle)
+    weight = mass * g
+    normal_force = get_normal_force(mass, ramp_angle, g)
+    friction_force = get_friction_force(mass, ramp_angle, materials, g)
+    down_ramp_comp = weight * math.sin(rad_angle)
+
+    return friction_force < down_ramp_comp
 
 
-def get_normal_force(m: float, incline: float, g: float = GRAVITY) -> float:
-    """Return the normal force of an object with mass m, on an ramp that is
-    incline degrees from the horizontal, with the given coefficients of
-    friction."""
+def get_friction_force(mass: float, incline: float, materials: List[str],
+                       g: float = GRAVITY) -> float:
+    """Return the friction force of an object with given mass, on a ramp that is
+    incline radians from the horizontal.
+
+    """
+    normal_force = get_normal_force(mass, incline, g)
+    mu = MATERIALS[materials[0]][materials[1]]
+
+    return normal_force * mu
+
+
+def get_normal_force(mass: float, incline: float, g: float = GRAVITY) -> float:
+    """Return the normal force of an object with given mass, on a ramp that is
+    incline radians from the horizontal.
+
+    Preconditions:
+        - 0 <= incline < math.pi / 2
+    """
     # todo: it'd make more sense for these two functions to give you components, collapse them?
-    weight = get_weight(m, g)
+    weight = mass * g
 
-    # weight time cos of the incline gives the component into the ramp
-    adj_component = math.cos(incline) * weight  # todo: does this do what i think it does?
+    # weight times cos of the incline gives the component into the ramp
+    adj_component = math.cos(incline) * weight
 
     return adj_component
 
 
-def get_weight_comps(m: float, incline: float = 0.0, g: float = GRAVITY) -> tuple[float, float, float]:
-    """Return the weight of the object with the given mass m, as well as components."""
-    weight = m * g
-
-    adj_component = math.cos(incline) * weight  # this is also normal force, going into ramp
-    opp_component = math.sin(incline) * weight  # component along ramp
-
-    return weight, adj_component, opp_component
-
-
-def object_slips(m: float, mu_obj: float, mu_ramp: float, incline: float = 0.0,
-                 g: float = GRAVITY) -> bool:
-    """Return whether an object of mass m will slip under given conditions.
-
-    Values of mu are from the coefficients.csv file. (They represent static friction.)
-    """
-    weight, normal_force, down_ramp_force = get_weight(m, incline, g)
-
-    # how does mu work again??
-    max_friction_force = mu_obj * normal_force
-
-    return max_friction_force <= down_ramp_force
+# def get_weight_comps(m: float, incline: float = 0.0, g: float = GRAVITY) -> \
+#         tuple[float, float, float]:
+#     """Return the weight of the object with the given mass m, as well as components."""
+#     weight = m * g
+#
+#     adj_component = math.cos(incline) * weight  # this is also normal force, going into ramp
+#     opp_component = math.sin(incline) * weight  # component along ramp
+#
+#     return weight, adj_component, opp_component
+#
+#
+# def object_slips(m: float, mu_obj: float, mu_ramp: float, incline: float = 0.0,
+#                  g: float = GRAVITY) -> bool:
+#     """Return whether an object of mass m will slip under given conditions.
+#
+#     Values of mu are from the coefficients.csv file. (They represent static friction.)
+#     """
+#     weight, normal_force, down_ramp_force = get_weight(m, incline, g)
+#
+#     # how does mu work again??
+#     max_friction_force = mu_obj * normal_force
+#
+#     return max_friction_force <= down_ramp_force
--- visualizations.py
+++ visualizations.py
@@ -1,8 +1,12 @@
 """Visualizing the friction simulator."""
 import math
+from typing import List
+
 import pygame
 from pygame.colordict import THECOLORS
-from basic_calculations import get_normal_force, get_ramp_height, get_ramp_length
+from basic_calculations import calculate_if_slips, get_friction_force, get_normal_force, \
+    get_ramp_height, \
+    get_ramp_length
 import interactions
 
 SCREEN_SIZE = 1000, 750  # width, then height
@@ -51,7 +55,8 @@ def draw_base_set_up(ramp_angle: float, mass: float) -> None:
         pygame.draw.polygon(screen, THECOLORS['salmon'], ramp_coords)
         pygame.draw.polygon(screen, THECOLORS['gold'], mass_coords)
 
-        draw_stats(screen, ramp_angle, mass)
+        draw_stats(screen, ramp_angle, mass, ['steel', 'steel'])
+        draw_about_slipping(screen, ramp_angle, mass, ['steel', 'steel'])
 
         pygame.display.flip()  # updates the display
 
@@ -66,7 +71,8 @@ def draw_base_set_up(ramp_angle: float, mass: float) -> None:
     pygame.display.quit()
 
 
-def draw_stats(screen: pygame.Surface, ramp_angle: float, mass: float) -> None:
+def draw_stats(screen: pygame.Surface, ramp_angle: float, mass: float,
+               materials: List[str]) -> None:
     """Draw the statistics in the top left corner of the screen.
 
     These stats include the angle of the ramp, the mass of the block in kg, and the
@@ -74,8 +80,11 @@ def draw_stats(screen: pygame.Surface, ramp_angle: float, mass: float) -> None:
     """
     pygame.draw.rect(screen, 'wheat1', (0, 0, screen.get_width() // 5, screen.get_height() // 3))
 
+    rad_angle = math.radians(ramp_angle)
     # this number of sig figs is arbitrary -- not really good science
-    normal_force_display = round(get_normal_force(mass, ramp_angle), 2)
+    # also note that these two assume the default value of gravity, 9.8
+    normal_force_display = round(get_normal_force(mass, rad_angle), 2)
+    friction_force_display = round(get_friction_force(mass, rad_angle, materials), 2)
 
     title_font = pygame.font.SysFont('arialrounded', 25)
     text_font = pygame.font.SysFont('arial', 20)
@@ -85,12 +94,34 @@ def draw_stats(screen: pygame.Surface, ramp_angle: float, mass: float) -> None:
     mass_text = text_font.render('Mass: ' + str(mass), True, (10, 10, 10))
     normal_text = text_font.render('Normal Force: ' + str(normal_force_display),
                                    True, (10, 10, 10))
+    friction_text = text_font.render('Friction Force: ' + str(friction_force_display),
+                                     True, (10, 10, 10))
 
     screen.blit(title_text, (1, 1))
-    screen.blit(angle_text, (1, (screen.get_height() // 3) // 4))
-    screen.blit(mass_text, (1, (screen.get_height() // 3) // 2))
-    screen.blit(normal_text, (1, (screen.get_height() // 3) * 0.75))
+    screen.blit(angle_text, (1, (screen.get_height() // 3) // 5))
+    screen.blit(mass_text, (1, (screen.get_height() // 3) * 0.4))
+    screen.blit(normal_text, (1, (screen.get_height() // 3) * 0.6))
+    screen.blit(friction_text, (1, (screen.get_height() // 3) * 0.8))
+
+
+def draw_about_slipping(screen: pygame.Surface, ramp_angle: float, mass: float,
+                        materials: List[str]) -> None:
+    """Draw the necessary text for showing whether the mass slips down the ramp, given
+    ramp_angle in degrees."""
+    title_font = pygame.font.SysFont('arialrounded', 28)
+    text_font = pygame.font.SysFont('arial', 24)
+
+    question_text = title_font.render('Does the mass slip?', True, (10, 10, 10))
+    answer = calculate_if_slips(mass, materials, ramp_angle)
+
+    if answer:
+        answer_text = text_font.render('Yes.', True, (10, 10, 10))
+    else:
+        answer_text = text_font.render('No.', True, (10, 10, 10))
+
+    screen.blit(question_text, (screen.get_width() / 2, 15))
+    screen.blit(answer_text, (screen.get_width() / 2 + 5, 55))
 
 
 if __name__ == '__main__':
-    draw_base_set_up(30, 25)
+    draw_base_set_up(5, 25)
